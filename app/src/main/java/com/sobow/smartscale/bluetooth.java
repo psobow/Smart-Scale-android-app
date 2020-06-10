a3a4cb6420fc95b2a8159ac0d5f9ab3c9622dc3e
Patryk Sobow <patryk.sobow95@gmail.com>
2020-06-10 18:36:19 +0200

    Create bluetooth layouts

---
+++ app/src/main/java/com/sobow/smartscale/bluetooth.java
@@ -0,0 +1,16 @@
+package com.sobow.smartscale;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class bluetooth extends AppCompatActivity
+{
+  
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_bluetooth);
+  }
+}
